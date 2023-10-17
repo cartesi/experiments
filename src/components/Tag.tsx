5a2da3bacc73e68d04f01b7b3ba711d603b50637
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2023-10-17 16:06:24 +0300

    Project types different colours

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,7 +2,6 @@ import Link from '@docusaurus/Link';
 import React from 'react';
 import { TagPathSeperator } from '../pages';
 import { PageFrontMatter } from '../theme/MDXContent';
-import { cn } from '../lib/utils';
 
 type Props = {
   tag: PageFrontMatter['tags'][keyof PageFrontMatter['tags']];
@@ -10,15 +9,58 @@ type Props = {
   isLink?: boolean;
 };
 
+type TagColours = {
+  option: string;
+  styles: string;
+};
+
+type Tag = {
+  label: keyof PageFrontMatter['tags'];
+  options: PageFrontMatter['tags'][keyof PageFrontMatter['tags']][];
+  colours: TagColours[];
+};
+
+export const projectStageColours = [
+  {
+    option: 'Stable release',
+    styles:
+      'cursor-pointer bg-secondary border-secondary text-secondary-foreground',
+  },
+  {
+    option: 'Release Candidate',
+    styles: 'cursor-pointer bg-tag1 border-tag1 text-secondary-foreground',
+  },
+  {
+    option: 'In progress - Beta',
+    styles: 'cursor-pointer bg-tag2 border-tag2 text-secondary-foreground',
+  },
+  {
+    option: 'In progress - Alpha',
+    styles: 'cursor-pointer bg-tag2 border-tag2 text-secondary-foreground',
+  },
+  {
+    option: 'In progress - Pre-Alpha',
+    styles: 'cursor-pointer bg-tag2 border-tag2 text-secondary-foreground',
+  },
+  {
+    option: 'Proof of concept',
+    styles: 'cursor-pointer bg-tag3 border-tag3 text-secondary-foreground',
+  },
+  {
+    option: 'Hackathon Project',
+    styles: 'cursor-pointer bg-tag4 border-tag4 text-secondary-foreground',
+  },
+];
+
 const TagContent = ({ tag, type }: Props) => {
   return (
     <span
-      className={cn(
-        'inline-flex text-center py-[.5em] px-[1em] bg-card text-card-foreground border border-solid border-[rgba(0,0,0,.2)] items-center rounded-full text-xs transition-colors font-heading uppercase tracking-wide',
-        type === 'projectStage'
-          ? 'cursor-pointer bg-secondary border-secondary text-secondary-foreground font-bold'
-          : 'cursor-default'
-      )}
+      className={`inline-flex text-center py-[.5em] px-[1em] border border-solid items-center rounded-full text-xs transition-colors uppercase tracking-wide font-heading
+        ${
+          type === 'projectStage'
+            ? projectStageColours.find(item => item.option === tag).styles
+            : 'cursor-default bg-card text-card-foreground border-[rgba(0,0,0,.2)]'
+        }`}
     >
       {tag}
     </span>
