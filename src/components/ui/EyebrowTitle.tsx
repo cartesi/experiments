fa66cd60b3e2042f4cb3a2961ab6715fcd5b23f2
Joro <joro.georgiev@gmail.com>
2024-09-03 12:11:39 +0300

    eyebrow fix

--- src/components/ui/EyebrowTitle.tsx
+++ src/components/ui/EyebrowTitle.tsx
@@ -15,7 +15,7 @@ const EyebrowTitle = ({ as, label, center = true, className }: Props) => {
     className: cn(
       `${
         center ? 'items-center' : ''
-      } before:bg-tertiary before:w-1/3 before:h-[0.3rem] flex flex-col gap-5 text-[0.75rem] md:text-sm uppercase tracking-wide text-tertiary font-semibold inline-flex font-heading mb-6 lg:mb-8`,
+      } before:content-[''] before:bg-tertiary before:w-1/3 before:h-[0.3rem] flex flex-col gap-5 text-[0.75rem] md:text-sm uppercase tracking-wide text-tertiary font-semibold inline-flex font-heading mb-6 lg:mb-8`,
       className
     ),
     children: label,
