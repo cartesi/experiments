09e2d7a57ceaa571b474637a955bd477e938de49
Joro <joro.georgiev@gmail.com>
2025-01-30 16:35:01 +0200

    refactor: share components

--- src/components/SocialShare.tsx
+++ src/components/SocialShare.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import React from 'react';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 import {
   EmailShareButton,
@@ -12,35 +12,37 @@ import {
   LinkedinIcon,
   TelegramIcon,
   EmailIcon,
-} from "react-share";
+} from 'react-share';
 
 type Props = {
   title: string;
 };
 
 const SocialShare = ({ title }: Props) => {
-  const url = ExecutionEnvironment.canUseDOM ? window.location.href : "";
+  const url = ExecutionEnvironment.canUseDOM ? window.location.href : '';
   return (
-    <>
-      <h4 className="mb-0 mt-4">Share project</h4>
-      <div className="flex gap-2 flex-wrap items-center">
-        <FacebookShareButton url={url} quote={title}>
+    <div className='flex gap-4 items-center'>
+      <h4 className='m-0 uppercase text-sm font-bold tracking-wide shrink-0'>
+        Share project
+      </h4>
+      <div className='flex gap-2 flex-wrap items-center'>
+        {/* <FacebookShareButton url={url} quote={title}>
           <FacebookIcon size={32} round />
-        </FacebookShareButton>
-        <TwitterShareButton url={url}>
-          <TwitterIcon size={32} round />
-        </TwitterShareButton>
-        <LinkedinShareButton url={url}>
-          <LinkedinIcon size={32} round />
-        </LinkedinShareButton>
-        <TelegramShareButton url={url}>
-          <TelegramIcon size={32} round />
-        </TelegramShareButton>
+        </FacebookShareButton> */}
         <EmailShareButton url={url}>
-          <EmailIcon size={32} round />
+          <img src='/img/share-link.svg' alt='email' />
         </EmailShareButton>
+        <TwitterShareButton url={url}>
+          <img src='/img/share-x.svg' alt='twitter' />
+        </TwitterShareButton>
+        {/* <LinkedinShareButton url={url}>
+          <LinkedinIcon size={32} round />
+        </LinkedinShareButton> */}
+        <TelegramShareButton url={url}>
+          <img src='/img/share-telegram.svg' alt='telegram' />
+        </TelegramShareButton>
       </div>
-    </>
+    </div>
   );
 };
 
