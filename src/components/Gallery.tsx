18c6ec6f85b90c24becd6f300326ddcaff386d92
Joro <joro.georgiev@gmail.com>
2025-02-11 10:50:05 +0200

    refactor gallery - add next/prev to lightbox, use swiper

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,6 +7,12 @@ import { useMediaQuery } from 'usehooks-ts';
 import IconChevron from './ui/IconChevron';
 import IconArrowLeft from './ui/IconArrowLeft';
 import IconArrowRight from './ui/IconArrowRight';
+import IconClose from './ui/IconClose';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 type Props = {
   thumbWidth?: string;
@@ -18,37 +24,15 @@ type Props = {
 
 const Gallery = ({ images, thumbWidth = '15rem' }: Props) => {
   const [showLightbox, setShowLightbox] = React.useState(false);
-  const [lightboxImage, setLightboxImage] = React.useState('');
+  const [swiper, setSwiper] = React.useState(null);
 
   const { siteConfig } = useDocusaurusContext();
 
   const location = useLocation();
 
-  const [scrollX, setScrollX] = React.useState(0);
-  const scrollContainer = React.useRef<HTMLDivElement>(null);
-
-  const handleImageClick = (image: string) => {
-    setLightboxImage(image);
+  const handleImageClick = (index: number) => {
     setShowLightbox(true);
-  };
-
-  const handleScroll = (direction: 'left' | 'right') => {
-    if (scrollContainer.current) {
-      const containerWidth = scrollContainer.current.clientWidth;
-      const scrollWidth = scrollContainer.current.scrollWidth;
-
-      if (direction === 'left') {
-        setScrollX(scrollX - containerWidth);
-      } else {
-        setScrollX(scrollX + containerWidth);
-      }
-
-      if (scrollX < 0) {
-        setScrollX(0);
-      } else if (scrollX + containerWidth > scrollWidth) {
-        setScrollX(scrollWidth - containerWidth);
-      }
-    }
+    swiper.slideTo(index);
   };
 
   React.useEffect(() => {
@@ -65,67 +49,104 @@ const Gallery = ({ images, thumbWidth = '15rem' }: Props) => {
     };
   }, []);
 
-  React.useEffect(() => {
-    if (scrollContainer.current) {
-      scrollContainer.current.scrollTo({
-        left: scrollX,
-        behavior: 'smooth',
-      });
-    }
-  }, [scrollX]);
-
   return (
     <div className='not-prose'>
-      {showLightbox && (
-        <div
-          className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70 backdrop-blur p-2 cursor-zoom-out'
-          style={{ zIndex: '100000' }}
+      <div
+        className='fixed inset-0 z-50 p-2 bg-black bg-opacity-70 backdrop-blur'
+        style={{ zIndex: '100000', display: showLightbox ? 'block' : 'none' }}
+      >
+        <button
+          className='z-20 text-white hover:bg-black/50 hover:text-white rounded-xl p-2 transition-opacity absolute right-4 top-4'
           onClick={() => setShowLightbox(false)}
         >
-          <div className='relative'>
-            <img src={lightboxImage} alt={``} />
-          </div>
+          <IconClose />
+        </button>
+
+        <div className='relative w-full h-full flex justify-center items-center'>
+          <button className='z-20 swiper-prev swiper-prev--lightbox text-white hover:bg-black/50 hover:text-white rounded-xl p-2 transition-opacity absolute left-4 top-1/2 transform -translate-y-1/2'>
+            <IconArrowLeft />
+          </button>
+          <button className='z-20 swiper-next swiper-next--lightbox text-white hover:bg-black/50 hover:text-white rounded-xl p-2 transition-opacity absolute right-4 top-1/2 transform -translate-y-1/2'>
+            <IconArrowRight />
+          </button>
+
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={0}
+            autoHeight={false}
+            className='mySwiper'
+            onSwiper={setSwiper}
+            navigation={{
+              nextEl: '.swiper-next--lightbox',
+              prevEl: '.swiper-prev--lightbox',
+            }}
+            modules={[Navigation]}
+          >
+            {images.map((image, i) => (
+              <SwiperSlide key={i}>
+                <img
+                  src={useBaseUrl(
+                    `${siteConfig.customFields.projectsImagesPath}${location.pathname}${image.image}`
+                  )}
+                  style={{ width: '100%' }}
+                  className='aspect-video object-cover shadow-sm m-0 max-w-none max-h-[90vh]'
+                  alt={image.alt}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-      )}
+      </div>
 
       <div className='flex gap-4 mb-4 justify-end -mt-6'>
-        <button
-          onClick={() => handleScroll('left')}
-          className='p-1 text-primary/50 transition-colors hover:text-primary'
-        >
+        <button className='swiper-prev swiper-prev--default hover:bg-white/5 rounded-xl p-2 transition-opacity'>
           <IconArrowLeft />
         </button>
-        <button
-          onClick={() => handleScroll('right')}
-          className='p-1 text-primary/50 transition-colors hover:text-primary'
-        >
+        <button className='swiper-next swiper-next--default hover:bg-white/5 rounded-xl p-2 transition-opacity'>
           <IconArrowRight />
         </button>
       </div>
 
-      <div
-        className='flex gap-4 snap-x overflow-x-auto scrollbar-hide'
-        ref={scrollContainer}
+      <Swiper
+        slidesPerView={1.5}
+        spaceBetween={16}
+        autoHeight={false}
+        className='mySwiper'
+        navigation={{
+          nextEl: '.swiper-next--default',
+          prevEl: '.swiper-prev--default',
+        }}
+        pagination={{
+          el: '.swiper-pagination',
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1.5,
+          },
+          768: {
+            slidesPerView: 2.5,
+          },
+          1024: {
+            slidesPerView: 2.5,
+          },
+        }}
+        modules={[Navigation, Pagination]}
       >
         {images.map((image, i) => (
-          <div key={i} className='snap-start'>
+          <SwiperSlide key={i}>
             <img
               src={useBaseUrl(
                 `${siteConfig.customFields.projectsImagesPath}${location.pathname}${image.image}`
               )}
-              style={{ width: thumbWidth }}
+              style={{ width: '100%' }}
               className='aspect-video object-cover shadow-sm cursor-zoom-in m-0 max-w-none'
               alt={image.alt}
-              onClick={() =>
-                handleImageClick(
-                  require(`@site/static/projects${location.pathname}${image.image}`)
-                    .default
-                )
-              }
+              onClick={() => handleImageClick(i)}
             />
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
