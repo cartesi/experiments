f27ee85f29300454629b70fbbd40d250a2b48510
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2025-02-28 20:38:37 +0200

    Upvoting feature

--- src/components/ShowcaseItem.tsx
+++ src/components/ShowcaseItem.tsx
@@ -4,6 +4,7 @@ import { PageFrontMatter } from '../theme/MDXContent';
 import Tag from './Tag';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Likes from '@site/src/components/Likes';
 
 type Props = {
   page: PageFrontMatter;
@@ -23,7 +24,7 @@ const ShowcaseItem = ({ page }: Props) => {
   return (
     <div className='flex flex-col gap-6 lg:gap-8 relative p-6 pb-4 bg-card text-card-foreground rounded-3xl'>
       <Link className='absolute inset-0 z-10' to={path} />
-      <div className='flex gap-4 items-center'>
+      <div className='flex gap-4 items-start'>
         <div className='overflow-hidden w-24 relative flex-shrink-0 rounded-2xl'>
           {thumbnail && (
             <img
@@ -36,11 +37,8 @@ const ShowcaseItem = ({ page }: Props) => {
             />
           )}
         </div>
-        <div>
-          {/* {JSON.stringify(tags)} */}
-          {tags.projectStage && (
-            <Tag type='projectStage' tag={tags.projectStage} />
-          )}
+        <div className='flex flex-row grow justify-end'>
+          <Likes path={path} />
         </div>
       </div>
       <div className='flex flex-col grow'>
@@ -51,6 +49,11 @@ const ShowcaseItem = ({ page }: Props) => {
             tag={tags.projectTypes[0]}
             contentClass='bg-transparent'
           />
+        </div>
+        <div className='pt-2 pb-4'>
+          {tags.projectStage && (
+            <Tag type='projectStage' tag={tags.projectStage} />
+          )}
         </div>
         <p className='text-sm mb-2 text-muted-foreground'>{description}</p>
         <div className='flex flex-wrap gap-1 mt-auto pt-2 relative z-20'>
